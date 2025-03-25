86646647e34cdce16d1c874a7c6d5530b032c531
Zulhan <zulhanfadil@gmail.com>
2025-03-25 21:34:17 +0700

    add search functionality and toggle visibility for search input and logo

--- sript.js
+++ sript.js
@@ -1,7 +1,17 @@
 const katalogTrigger = document.getElementById('tombolkatalog');
 const katalogDropdown = document.getElementById('katalog');
 
+const searchTrigger = document.getElementById('tombolsearch');
+const searchInput = document.getElementById('search');
+const logo = document.getElementById('logo');
+
 katalogTrigger.addEventListener('click', (e) => {
   e.preventDefault();
   katalogDropdown.classList.toggle('show');
+});
+
+searchTrigger.addEventListener('click', (e) => {
+  e.preventDefault();
+  searchInput.classList.toggle('show');
+  logo.classList.toggle('hide');
 });
